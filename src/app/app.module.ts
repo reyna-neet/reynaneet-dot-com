aae31083048967dcdf6781f966bec81605ea50d4
Reyna Neet <reyna.neet@gmail.com>
2022-11-12 02:23:16 -0600

    Added a fading animation when the quote changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,8 +14,9 @@ import { QuotesComponent } from './quotes/quotes.component';
     QuotesComponent,
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -1,31 +1,59 @@
 import { Component, OnInit } from '@angular/core';
+import { AnimationEvent, animate, transition, 
+         trigger, state, style } from '@angular/animations';
 import { interval, Subscription } from 'rxjs';
 import { Quote } from '../quote';
 import { QuoteService } from '../quote.service';
 
 @Component({
   selector: 'app-quotes',
+  animations: [
+    trigger('fadeAndReturn', [
+      state('visible', style({
+        opacity: 1
+      })),
+      state('invisible', style({
+        opacity: 0
+      })),
+      transition('visible => invisible', [
+        animate(1000)
+      ]),
+      transition('invisible => visible', [
+        animate(1000)
+      ]),
+    ]),
+  ],
   templateUrl: './quotes.component.html',
-  styleUrls: ['./quotes.component.scss']
+  styleUrls: ['./quotes.component.scss'],
 })
+
 export class QuotesComponent implements OnInit {
   QUOTE_CHANGE_DELAY = 10000;
+  ANIMATION_LENGTH = 1000;
 
   quotes: Quote[] = [];
   displayQuoteIndex = 0;
+  isVisible = true;
   quoteTimer: Subscription = interval(this.QUOTE_CHANGE_DELAY)
-                             .subscribe(val => this.nextQuote());
+                             .subscribe(val => this.quoteFade());
   
   getQuotes(): void {
     this.quotes = this.quoteService.getQuotes();
   }
 
-  nextQuote(): void {
-    if (this.displayQuoteIndex == this.quotes.length) {
-      this.displayQuoteIndex = 0;
-    }
-    else {
-      this.displayQuoteIndex++;
+  quoteFade(): void {
+    this.isVisible = false;
+  }
+
+  onQuoteFadeOut(event: AnimationEvent) {
+    if (!this.isVisible) {
+      if (this.displayQuoteIndex == this.quotes.length-1) {
+        this.displayQuoteIndex = 0;
+      }
+      else {
+        this.displayQuoteIndex++;
+      }
+      this.isVisible = true;
     }
   }
 
